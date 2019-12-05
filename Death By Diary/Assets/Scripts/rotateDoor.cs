e6f33b7a0efee9930ce2cf5dd47397451389d46f
Anttya <36339934+Anttya@users.noreply.github.com>
2019-12-05 08:28:45 -0500

    Added Code

---
+++ Death By Diary/Assets/Scripts/rotateDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class rotateDoor : MonoBehaviour
+{
+    //door to rotate
+    public GameObject dooraxis;
+
+    //degree to rotate to
+    public int degree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dooraxis.transform.rotation = Quaternion.Euler(dooraxis.transform.rotation.x, degree, dooraxis.transform.rotation.z);
+    }
+}
